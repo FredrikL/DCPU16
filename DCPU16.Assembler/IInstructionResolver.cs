3822e251cd01dd583b5d4dd1939db2a7333ccb99
Fredrik Leijon <fredrik.leijon@gmail.com>
2012-04-25 08:32:24 +0200

    cleanup

--- DCPU16.Assembler/IInstructionResolver.cs
+++ DCPU16.Assembler/IInstructionResolver.cs
@@ -4,7 +4,6 @@ namespace DCPU16.Assembler
 {
     public interface IInstructionResolver
     {
-        dynamic Resolve(string statement);
         Tuple<ushort, ushort> ResolveRegisterPointer(string hex, ushort reg);
         Tuple<ushort, ushort> ResolveNextWord(string hex);
         dynamic ResolveHex(string hex);
--- DCPU16.Assembler/InstructionResolver.cs
+++ DCPU16.Assembler/InstructionResolver.cs
@@ -1,56 +1,11 @@
 using System;
 using System.Globalization;
-using System.Text.RegularExpressions;
 
 namespace DCPU16.Assembler
 {
     public class InstructionResolver : IInstructionResolver
     {
-        private IValueMap valueMap = new ValueMap();
-
-        public dynamic Resolve(string statement)
-        {
-            // item 1 instruction (a|b)
-            // item 2 value for next word
-            Match match;// = Regex.Match(statement, @"\[0x(\d{4})\+([ABCXYZIJ])\]",RegexOptions.Compiled);
-            //if(match.Success)
-            //{
-            //    ushort first = (ushort)(this.valueMap[match.Groups[2].Value] + 0x10);
-            //    ushort second = UInt16.Parse(match.Groups[1].Value, NumberStyles.HexNumber);
-            //    return new Tuple<ushort, ushort>(first,second);
-            //}
-
-            //match = Regex.Match(statement, @"\[0x(\d{4})\]",RegexOptions.Compiled);
-            //if (match.Success)
-            //{
-            //    ushort first = (ushort)(0x1e);
-            //    ushort second = UInt16.Parse(match.Groups[1].Value, NumberStyles.HexNumber);
-            //    return new Tuple<ushort, ushort>(first, second);
-            //}
-           
-            //match = Regex.Match(statement, @"^0x(\d{1,4})|(\d{1,4})$", RegexOptions.Compiled);
-            //if (match.Success)
-            //{
-            //    ushort first = (ushort)(0x1f);
-            //    ushort second = UInt16.Parse(match.Groups[1].Value, NumberStyles.HexNumber);
-
-            //    // Handle literal values 
-            //    if(second <= 0x1f) 
-            //        return (ushort) (second + 0x20);
-
-            //    return new Tuple<ushort, ushort>(first, second);
-            //}
-
-            match = Regex.Match(statement, @"\[([\w\d_]+)\+([ABCXYZIJ])\]", RegexOptions.Compiled);
-            if (match.Success)
-            {
-                ushort value = (ushort)(this.valueMap[match.Groups[2].Value] + 0x10);
-                string label = match.Groups[1].Value;
-
-                return new Tuple<ushort, string>(value, label);
-            }
-            throw new ArgumentException("Que?");
-        }
+        private IValueMap valueMap = new ValueMap();       
 
         public Tuple<ushort, ushort> ResolveRegisterPointer(string hex, ushort reg)
         {
